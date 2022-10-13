191e37e96014514c94ffa536cf3d469163e2f970
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-10-13 17:07:56 +0200

    Business section completed

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -1,5 +1,60 @@
+import { features } from "../constants/data";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+type Feature = {
+  id: string;
+  icon: string;
+  title: string;
+  content: string;
+  index: number;
+};
+
+const FeatureCard = ({ icon, title, content, index }: Feature) => (
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? "mb-6" : "mb-0"
+    } feature-card`}
+  >
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+    >
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-nornal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
+    </div>
+  </div>
+);
+
 const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Lorem ipsum dolor sit <br className="sm:block hide" /> amet
+          consectetur adipisicing elit.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod unde
+          consectetur ab dolores deleniti in quidem aliquid voluptatem,
+          recusandae dolorum nobis esse omnis provident. Veniam consequuntur nam
+          nostrum praesentium illum!
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Business;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,16 @@
-function Button() {
-  return <div>Button</div>;
-}
+type Props = {
+  styles: string;
+};
+
+const Button = ({ styles }: Props) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get started
+    </button>
+  );
+};
 
 export default Button;
